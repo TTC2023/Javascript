a1a4c6e0bb2dea3f5006b5e5bb89dab745ff8118
Tyler Correll <corrett10@gmail.com>
2023-02-28 13:12:33 -0500

    closer to skywalker api

--- practice/apiwalker/src/App.js
+++ practice/apiwalker/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import {useNavigate} from 'react-router-dom';
+
 
 function App() {
+  const [type, setType] = useState("people")
+  const [id, setId] = useState(1)
+  const [display, setDisplay] = useState("")
+  // const navigate = useNavigate()
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/${type}/${id}`)
+      .then(res => setDisplay(JSON.stringify(res.data)))
+      .catch(err => console.log(err))
+  }, [type, id]);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    // navigate(`/${type}/${id}`)
+  }
+
   return (
-    <div >
-     
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <label>Type</label>
+        <input type="text" value={type} onChange={(e) => setType(e.target.value)} />
+        <label>Id</label>
+        <input type="number" value={id} onChange={(e) => setId(e.target.value)} />
+        <button>Enter</button>
+      </form>
+      <p>{display}</p>
     </div>
   );
 }
