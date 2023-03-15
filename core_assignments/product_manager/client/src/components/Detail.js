617edf10e13ee2327628d3528815596fc7a65b11
Tyler Correll <corrett10@gmail.com>
2023-03-15 09:11:49 -0400

    updated

--- core_assignments/product_manager/client/src/components/Detail.js
+++ core_assignments/product_manager/client/src/components/Detail.js
@@ -1,10 +1,19 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 const Detail = (props) => {
     const [product, setProduct] = useState({})
     const {id} = useParams()
+    const navigate = useNavigate()
+
+    const deleteProduct = (productId) => {
+      axios.delete('http://localhost:8000/api/products/' + productId)
+          .then((res) => {
+              navigate('/products')
+          })
+          .catch(err => console.log(err))
+  }
     useEffect(() => {
         axios.get("http://localhost:8000/api/products/" + id)
             .then( res => {
@@ -18,6 +27,7 @@ const Detail = (props) => {
         <p>Title: {product.title}</p>
         <p>Price: {product.price}</p>
         <p>Description: {product.description}</p>
+        <input type="submit" value="Delete" onClick={(e) => (deleteProduct(product._id))}/>
     </div>
   )
 }
