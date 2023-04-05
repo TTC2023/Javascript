89fc8aad7079e7cb6b7d83d7635c0e5cdca206e9
Tyler Correll <corrett10@gmail.com>
2023-04-05 16:49:43 -0500

    Arrays to do 1

--- algos/core/arraysToDo1.js
+++ algos/core/arraysToDo1.js
@@ -24,11 +24,99 @@ pushFront([5,7,2,3,6,2], 8)
 // popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
 
 function popFront(arr){
-    newArr = []
-    for(i=1; i < arr.length; i++){
+    let newArr = []
+    for(let i=1; i < arr.length; i++){
         newArr.push(arr[i])
     }
     console.log(newArr)
     return arr[0]
 }
 console.log(popFront([4,5,7,9]))
+
+// Insert At
+// Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). You may use .push(), you are able do this without it though!
+
+// Examples:
+
+// insertAt([100,200,5], 2, 311) => [100,200,311,5]
+// insertAt([9,33,7], 1, 42) => [9,42,33,7]
+
+function insertAt(arr,index,num){
+    arr.splice(index,0,num)
+    return arr
+}
+console.log(insertAt([100,200,5], 2, 311))
+
+// BONUS: Remove At
+// Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it. Think of popFront(arr) as equivalent to removeAt(arr,0).
+
+// Examples:
+
+// removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+// removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
+
+function removeAt(arr, index) {
+    let newArr = [];
+    let num = null;
+    for (let i = 0; i < arr.length; i++) {
+        if (i == index) {
+            num = arr[i];
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+    return num;
+}
+console.log(removeAt([1000, 3, 204, 77], 1));
+
+// BONUS: Swap Pairs
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element.
+
+// Examples:
+
+// insertAt([1,2,3,4]) => [2,1,4,3]
+// insertAt(["Brendan",true,42]) => [true,"Brendan",42]
+
+function swapPairs(arr){
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if(i%2==1){
+            newArr[i]=arr[i-1]
+            newArr[i-1]=arr[i]
+        }
+        else{
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+console.log(swapPairs(["Brendan",true,42]))
+
+// BONUS: Remove Duplicates
+// Given a sorted array, remove duplicate values. Because array elements are already in order, 
+// all duplicate values will be grouped together. 
+// If you already made the Remove At function, 
+// you are welcome to use that! If you solved this using nested loops, 
+// for an extra challenge, try to do it without any nested loops!
+
+// Examples:
+
+// removeDupes([-2,-2,3.14,5,5,10]) => [-2,3.14,5,10]
+// removeDupes([9,19,19,19,19,19,29]) => [9,19,29]
+
+function removeDupes(arr){
+    let existing = 0
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]!= existing){
+            newArr.push(arr[i])
+            existing = arr[i]
+        }
+        else{
+            existing = arr[i]
+        }
+    }
+    return newArr
+}
+console.log(removeDupes([9,19,19,19,19,19,29]))
