9f32a0a26b1d2a827959df93c9c38ce1214d7981
Tyler Correll <corrett10@gmail.com>
2023-04-13 16:48:10 -0500

    solved problem

--- algos/practice/python/leetcode.py
+++ algos/practice/python/leetcode.py
@@ -1,11 +1,47 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def mergeTwoLists(self, list1, list2):
-        list1.val = self.head
-        list2.val = self.next
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-node1 = ListNode()
-node1 = Solution([1,2],[2,3])
+# def strStr(haystack, needle):
+#     # find which word is longer
+#     if len(needle) > len(haystack):
+#         return -1
+#     firstIndex = None
+#     matches = ""
+#     needleIndex = 0
+#     # loop through and push each index if letters match
+#     for i in range(len(haystack)):
+#         if haystack[i] == needle[needleIndex]:
+#             if firstIndex == None:
+#                 # finds the first index
+#                 firstIndex = i
+#             # appends to matches
+#             matches += haystack[i]
+#             if needleIndex < len(needle):
+#                 needleIndex += 1
+#             else:
+#                 break
+#             # checks to see if they match
+#             if matches == needle:
+#                 return firstIndex
+#         else:
+#             firstIndex=None
+#             matches = ""
+#     return -1
+# print(strStr("hello", "ll"))
+
+def strStr(haystack, needle):
+    firstIndex = None
+    matches = ""
+    for i in range(len(haystack)):
+        for j in range(len(needle)):
+            if firstIndex == None:
+                firstIndex = i
+            if haystack[i] == needle[j]:
+                matches += haystack[i]
+            if matches == needle:
+                return firstIndex
+    return -1
+
+print(strStr("hello", "ll"))
+
+
+
