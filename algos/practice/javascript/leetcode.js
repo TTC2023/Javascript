6c4fc7109201fcc6bf1806f363d44e3455d3389b
Tyler Correll <corrett10@gmail.com>
2023-05-18 09:50:58 -0600

    createCounter

--- algos/practice/javascript/leetcode.js
+++ algos/practice/javascript/leetcode.js
@@ -17,3 +17,21 @@ var once = function(fn) {
 let fn = (a,b,c) => (a + b + c);
 let onceFn = once(fn);
 console.log(onceFn(1, 2, 3)); // logs 6
+
+var createCounter = function(init) {
+    let original = init
+    return {
+        increment: function () {
+            init++
+            return init
+        },
+        decrement: function (){
+            init--
+            return init
+        },
+        reset: function (){
+            init = original
+            return init
+        }
+    } 
+};
