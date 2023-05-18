455c3431c838aa15a2e5d453ccf89dcfe14c3010
Tyler Correll <corrett10@gmail.com>
2023-05-18 10:16:40 -0600

    algo

--- algos/practice/javascript/leetcode.js
+++ algos/practice/javascript/leetcode.js
@@ -35,3 +35,11 @@ var createCounter = function(init) {
         }
     } 
 };
+
+var map = function(arr, fn) {
+    let res = []
+    for(let i=0; i < arr.length; i++){
+        res.push(fn(arr[i], i))
+    }
+    return res
+};
