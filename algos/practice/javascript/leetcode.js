22d2e9e3f604a7cf7c83f234c7f4f277b96bbb14
Tyler Correll <corrett10@gmail.com>
2023-05-19 10:35:37 -0600

    js algos

--- algos/practice/javascript/leetcode.js
+++ algos/practice/javascript/leetcode.js
@@ -43,3 +43,13 @@ var map = function(arr, fn) {
     }
     return res
 };
+
+var filter = function(arr, fn) {
+    const res = []
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i],i)){
+            res.push(arr[i])
+        }
+    }
+    return res
+};
