7aa6867c27567875e14cc49098127c3c8b7ec9b8
Tyler Correll <corrett10@gmail.com>
2023-05-25 12:05:24 -0600

    still need to work on operators

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Bottom from './components/Bottom.jsx'
+import Bottom from './components/Bottom/Bottom.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Display from './components/Display';
 import {useState} from 'react'
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [operator, setOperator] = useState('')
+  const [equation, setEquation] = useState('');
+  const [answer, setAnswer] = useState('')
 
   return (
     <div className='d-flex flex-column align-items-center mx-auto' style={{height: '400px', backgroundColor: 'red', width: '350px', padding: '4px', margin: '15%'}}>
       <div className='d-flex flex-column align-items-center m-4' style={{ height: '100vh' }}>
-        <Display number={number}/>
-        <Bottom setNumber={setNumber}/>
+        <Display equation={equation} answer={answer}/>
+        <Bottom setEquation={setEquation} setAnswer={setAnswer} equation={equation}/>
       </div>
     </div>
   );
--- calculator/src/components/Display.jsx
+++ calculator/src/components/Display.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 
-const Display = ({number}) => {
+const Display = ({equation, answer}) => {
 
   return (
-    <div style={{height: '50px', width: '300px', backgroundColor: 'black'}}>
-        <p style={{color: 'white'}}>{number}</p>
+    <div>
+      <div className="d-flex justify-content-between" style={{height: '50px', width: '300px', backgroundColor: 'black'}}>
+        <p className='m-1' style={{color: 'white'}}>Equation</p>
+        <p className='m-1' style={{color: 'white'}}>Answer</p>
+      </div>
+      <div className="d-flex justify-content-between" style={{height: '50px', width: '300px', backgroundColor: 'black'}}>
+        <p className='m-3' style={{color: 'white'}}>{equation}</p>
+        <p className='m-3' style={{color: 'white'}}>{answer}</p>
+      </div>
     </div>
   )
 }
