0eaddcab7d69dd9f2fb05b6023644ca1c7f07575
Tyler Correll <corrett10@gmail.com>
2023-06-09 14:20:40 -0600

    algo

--- algos/practice/javascript/leetcode.js
+++ algos/practice/javascript/leetcode.js
@@ -177,3 +177,16 @@ var maximum69Number = function(num) {
     console.log(arr)
     return Math.max(...arr);
   };
+
+  var countKDifference = function(nums, k) {
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if(Math.abs(nums[i] - nums[j])===k){
+                count++
+            };
+        }
+    }
+    return count      
+    };
