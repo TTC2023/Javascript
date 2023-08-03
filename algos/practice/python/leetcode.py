6926b83cb3b0a259b7d3724ce34e359bb417ca10
Tyler Correll <corrett10@gmail.com>
2023-08-03 09:39:33 -0500

    alog

--- algos/practice/python/leetcode.py
+++ algos/practice/python/leetcode.py
@@ -633,6 +633,13 @@ def reverseWords(self, s: str) -> str:
             word = ""
         return ans
 
+def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        ans = 0
+        for i in range(len(hours)):
+            if hours[i] >= target:
+                ans +=1
+        return ans
+
 
 
 
